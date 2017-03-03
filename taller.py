804da57fa932835659a20b989573d1430aeb52d7
arleysebastianseguravargas <sebastian-arley@live.com>
2017-03-02 20:34:26 -0500

    taller1
    http://www.codeskulptor.org/#user42_gRHteahS0xBBnN1.py

---
+++ taller.py
@@ -0,0 +1,11 @@
+for i in range(20,0,-1):
+    print(i)
+print("qui termina el primer punto")
+
+cadena="una cadena de caracteres usando las estructuras de control que crea convenientes"
+z=0
+for i in cadena:
+    if (i=="a"):
+        z=z+1
+print("el totalde a es ")
+print(z)
